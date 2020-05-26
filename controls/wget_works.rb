d12766d3dec4b7640262874f739c14309d7cbf76
Davy McAleer <dmcaleer@chef.io>
2020-05-26 11:50:28 +0100

    attributes to inputs and extra InSpec conditions
    Signed-off-by: Davy McAleer <dmcaleer@chef.io>

--- controls/wget_works.rb
+++ controls/wget_works.rb
@@ -1,5 +1,7 @@
-wget_test = attribute('wget_test', default: '/bin/wget --version')
+wget_test = input('wget_test', value: '/bin/wget --version')
 
 describe bash(wget_test) do
   its('stdout') { should match /GNU/ }
+  its('stderr') { should eq '' }
+  its('exit_status') { should eq 0 }
 end
